23a7f88a8550d4cadd6f6235b1db2da2d4b0a278
Nikita Joshi <nikita.joshi@expleogroup.com>
2022-11-23 13:19:57 +0530

    Initial push

---
+++ Project/internal/repository/dbrepo/dbrepo.go
@@ -0,0 +1,31 @@
+package dbrepo
+
+import (
+	"database/sql"
+
+	"github.com/JoshiNikita17/udemyproject1/internal/config"
+	"github.com/JoshiNikita17/udemyproject1/internal/repository"
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
+
+func NewTestingsRepo(a *config.AppConfig) repository.DatabaseRepo {
+	return &testDBRepo{
+		App: a,
+	}
+}
---
+++ Project/migrations/20221123053102_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO "public"."users" ("first_name", "last_name", "email", "password", "access_level", "created_at", "updated_at") 
+VALUES ('Nikta', 'Joshi', 'nikita@gmail.com', '$2a$12$o0z9S.BtYU/2frzIteSlq.AHFBxTtUDxowt4ghyHWpHpF2tSFVmMW', '3', '2020-12-04', '2020-12-04');
